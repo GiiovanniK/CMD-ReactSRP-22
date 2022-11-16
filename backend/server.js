849c42834af60abc82e979ea70b8629836c1822b
Giovanni Kornet <giovannibrandonkornet@gmail.com>
2022-11-16 10:27:36 +0100

    Fixed connection to DB

--- backend/server.js
+++ backend/server.js
@@ -11,25 +11,37 @@ app.use(cors());
 
 const port = process.env.PORT || 5000;
 const API_URL = process.env.API_URL;
+const API_KEY = process.env.API_KEY;
 const DATABASE = process.env.DATABASE;
 const DATASOURCE = process.env.DATASOURCE;
 
-app.get("/", (req, res) => {
+const config = {
+  headers: {
+    "content-type": "application/json",
+    "api-key": API_KEY,
+  },
+};
+
+const payload = {
+  collection: "users",
+  database: DATABASE,
+  dataSource: DATASOURCE,
+  projection: {
+    _id: 1,
+  },
+};
+
+app.get("/", (_, res) => {
   res.send("Hello World!");
 });
 
-// the route we're working with
-app.get("/users", (req, res) => {
-  axios.post(API_URL, {
-      collection: "users",
-      database: DATABASE,
-      dataSource: DATASOURCE,
-
-      projection: {
-        _id: 1,
-      },
-    })
-    .then((response) => res.send(response.data));
+app.get("/users", (_, res) => {
+  axios
+    .post(API_URL + "/action/findOne", payload, config)
+    .then((response) => res.send(response.data))
+    .catch((error) => {
+      console.log(error);
+    });
 });
 
 // console log when app is running
