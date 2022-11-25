aa811575da471f94c714ecc7d23daabc0c8304b4
Giovanni. K <giovannibrandonkornet@gmail.com>
2022-11-25 16:28:45 +0100

    Updated protected routes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,47 +1,47 @@
-import { Route } from 'react-router-dom';
-import {
-  IonApp,
-  setupIonicReact
-} from '@ionic/react';
-import { IonReactRouter } from '@ionic/react-router';
+import { Route } from "react-router-dom";
+import { IonApp, setupIonicReact } from "@ionic/react";
+import { IonReactRouter } from "@ionic/react-router";
 
 /* Core CSS required for Ionic components to work properly */
-import '@ionic/react/css/core.css';
+import "@ionic/react/css/core.css";
 
 /* Basic CSS for apps built with Ionic */
-import '@ionic/react/css/normalize.css';
-import '@ionic/react/css/structure.css';
-import '@ionic/react/css/typography.css';
+import "@ionic/react/css/normalize.css";
+import "@ionic/react/css/structure.css";
+import "@ionic/react/css/typography.css";
 
 /* Optional CSS utils that can be commented out */
-import '@ionic/react/css/padding.css';
-import '@ionic/react/css/float-elements.css';
-import '@ionic/react/css/text-alignment.css';
-import '@ionic/react/css/text-transformation.css';
-import '@ionic/react/css/flex-utils.css';
-import '@ionic/react/css/display.css';
+import "@ionic/react/css/padding.css";
+import "@ionic/react/css/float-elements.css";
+import "@ionic/react/css/text-alignment.css";
+import "@ionic/react/css/text-transformation.css";
+import "@ionic/react/css/flex-utils.css";
+import "@ionic/react/css/display.css";
 
 /* Theme variables */
-import './theme/variables.css';
-import Home from './pages/home/Home';
-import Register from './pages/user/Register';
-import Login from './pages/user/Login';
-import Dashboard from './pages/admin/Dashboard';
-import ProtectedRoute from './services/ProtectService';
+import "./theme/variables.css";
+import Home from "./pages/home/Home";
+import Register from "./pages/user/Register";
+import Login from "./pages/user/Login";
+import Dashboard from "./pages/admin/Dashboard";
 
 setupIonicReact();
 
-const App: React.FC = () => ( 
-    <IonApp>
-      <IonReactRouter>
-        <ProtectedRoute>
-          <Route path="/home" component={Home} exact={true} />
-        </ProtectedRoute>        
-        <Route path="/register" component={Register} exact={true} />
-        <Route path="/login" component={Login} exact={true} />
-        <Route path="/dashboard" component={Dashboard} exact={true} />
-      </IonReactRouter>
-    </IonApp>
-  );
+const App: React.FC = () => (
+  <IonApp>
+    <IonReactRouter>
+      <Route exact path="/home" render={() => {
+          return localStorage.getItem("user") ? <Home /> : <Login />;
+        }}
+      />
+      <Route exact path="/dashboard" render={() => {
+          return localStorage.getItem("user") ? <Dashboard /> : <Login />;
+        }}
+      />
+      <Route path="/register" component={Register} exact={true} />
+      <Route path="/login" component={Login} exact={true} />      
+    </IonReactRouter>
+  </IonApp>
+);
 
 export default App;
